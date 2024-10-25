28e31a42289838905278eb82ab29ba3a9ecff803
Your Name <khalidsaifullaha3@gmail.com>
2024-10-25 09:21:59 +0600

    added banner section

--- src/assets/components/Header/Banner/Banner.jsx
+++ src/assets/components/Header/Banner/Banner.jsx
@@ -1,9 +1,18 @@
+import bg from '../../../asset/bg-shadow.png'
+import mainBg from '../../../asset/banner-main.png'
+
 const Banner = () => {
     return (
-        <div className="w-10/12 mx-auto object-cover relative mb-16 rounded-3xl">
-            {/* <img src="https://i.ibb.co.com/n8fBzzt/bg-shadow.png" alt="" /> */}
-            {/* <img src="../../../asset/bg-shadow.png" alt="" /> */}
-            {/* <img className="h-28 w-28 " src="https://i.ibb.co.com/tm9qZ4V/banner-main.png" alt="" /> */}
+        <div className=" w-10/12 mx-auto bg-cover  h-[600px] rounded-xl bg-black" style={{ backgroundImage: `url(${bg})` }}>
+            <div className='flex  justify-center '>
+            <img  src={mainBg} alt="" />
+            
+            </div>
+            <div className='text-center'>
+            <h2 className='text-4xl text-white'>Assemble Your Ultimate Dream 11 Cricket Team</h2>
+            <p className='text-2xl text-gray-300 font-semibold'>Beyond Boundaries Beyond Limits</p>
+            <button className='btn bg-pink-500 text-white font-semibold '>Claim Free Credit</button>
+            </div>
         </div>
     );
 };
